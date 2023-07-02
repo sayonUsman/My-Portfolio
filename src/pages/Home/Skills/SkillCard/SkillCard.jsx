cf03a2e9c274e782bc4a7bbd5d943bd395a8ed86
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-03 00:04:22 +0300

    UI design has been updated for extra small devices

--- src/pages/Home/Skills/SkillCard/SkillCard.jsx
+++ src/pages/Home/Skills/SkillCard/SkillCard.jsx
@@ -7,7 +7,7 @@ const SkillCard = ({ logo, title }) => {
   return (
     <div className="flex mx-auto mt-5">
       <div
-        className="card card-compact w-64 h-64 bg-white border border-black mb-5"
+        className="card card-compact w-40 h-40 sm:w-64 sm:h-64 bg-white border border-black mb-5"
         data-aos="zoom-in-down"
         data-aos-duration="2000"
         data-aos-easing="ease-in-out"
@@ -15,7 +15,11 @@ const SkillCard = ({ logo, title }) => {
         <div className="flex justify-center items-center h-full">
           <figure className="flex mx-auto my-auto">
             <div>
-              <img src={logo} alt="logo" className="w-36 h-36" />
+              <img
+                src={logo}
+                alt="logo"
+                className="w-24 h-24 sm:w-36 sm:h-36"
+              />
               <h2 className="text-lg text-center font-semibold pt-1">
                 {title}
               </h2>
--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -45,7 +45,7 @@ const logos = [
 
 const Skills = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-3 md:mt-5 md:px-16">
+    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-3 md:mt-5 md:px-16">
       {logos.slice(0, 8).map((logo) => (
         <SkillCard
           key={logos.indexOf(logo)}
