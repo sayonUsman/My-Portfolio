facf20539a6ee594783a4b74af34ed8954665a0f
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-04 03:50:56 +0300

    Added projects section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import About from "./About/About";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
 
 const Home = () => {
   AOS.init();
@@ -33,11 +34,25 @@ const Home = () => {
         data-aos-easing="ease-in-out"
       >
         <h1 className="text-3xl sm:text-4xl xl:text-5xl font-semibold">
-          Skills
+          Field of My Skills
         </h1>
       </div>
 
       <Skills></Skills>
+
+      <div
+        id="projects"
+        className="mt-10 md:mt-12 lg:mt-14 pl-7 sm:pl-9"
+        data-aos="fade-right"
+        data-aos-duration="1500"
+        data-aos-easing="ease-in-out"
+      >
+        <h1 className="text-3xl sm:text-4xl xl:text-5xl font-semibold">
+          Projects
+        </h1>
+      </div>
+
+      <Projects></Projects>
     </>
   );
 };
---
+++ src/pages/Home/Projects/Projects.jsx
@@ -0,0 +1,48 @@
+import art from "../../../assets/projects/art-craft.png";
+import bistro from "../../../assets/projects/bistro-boss.png";
+import disney from "../../../assets/projects/disney-empire.png";
+import ProjectCard from "./ProjectCard/ProjectCard";
+
+const projects = [
+  {
+    name: "Fun with Art & Craft",
+    demo: art,
+    client: "https://github.com/sayonUsman/Fun-with-Art-Craft",
+    server: "https://github.com/sayonUsman/Fun-with-Art-Craft-Server",
+    live: "https://fun-with-art-craft.web.app/",
+    tt: "React, Tailwind CSS, DaisyUI, Express.js, MongoDB, Firebase, Git, Stripe",
+  },
+
+  {
+    name: "Disney Empire",
+    demo: disney,
+    client: "https://github.com/sayonUsman/Disney-Empire",
+    server: "https://github.com/sayonUsman/Disney-Empire-Server",
+    live: "https://disney-empire.web.app/",
+    tt: "React, Tailwind CSS, DaisyUI, Express.js, MongoDB, Firebase, Git",
+  },
+
+  {
+    name: "Bistro Boss Restaurant",
+    demo: bistro,
+    client: "https://github.com/sayonUsman/Bistro-Boss-Restaurant",
+    server: "https://github.com/sayonUsman/Bistro-Boss-Restaurant-Server",
+    live: "https://bistro-boss-restaurant-2cd47.web.app/",
+    tt: "React, Tailwind CSS, DaisyUI, Express.js, MongoDB, Firebase, Git, Stripe",
+  },
+];
+
+const Projects = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 mt-5">
+      {projects.map((project) => (
+        <ProjectCard
+          key={projects.indexOf(project)}
+          project={project}
+        ></ProjectCard>
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
