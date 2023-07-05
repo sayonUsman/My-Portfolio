57b773d3b45a0f2498a480569116d60032f4dcc1
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-05 23:51:27 +0300

    Hover effect has been removed from logo card

--- src/pages/Home/Skills/LogoCard/LogoCard.jsx
+++ src/pages/Home/Skills/LogoCard/LogoCard.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const LogoCard = ({ logo }) => {
   return (
     <div className="flex mx-auto">
-      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+      <motion.div whileTap={{ scale: 0.8 }}>
         <div className="card card-compact w-24 h-24 bg-white shadow-md shadow-zinc-900 m-3 hover:hover:shadow-orange-400">
           <div className="flex justify-center items-center h-full">
             <figure className="flex mx-auto my-auto">
