de345321c2b5db2128d281261b99f2cbf749ce73
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-05 18:26:19 +0300

    Added footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../pages/Shared/Navbar/Navbar";
+import Navbar from "../pages/Navbar/Navbar";
+import Footer from "../pages/Footer/Footer";
 
 const Main = () => {
   return (
     <div className="container mx-auto">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-white text-black mb-3">
+      <p className="sm:text-lg">
+        Copyright © 2023 - All right reserved by Sayon Usman
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
