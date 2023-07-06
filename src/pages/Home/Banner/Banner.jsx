2c16d9e243c7566af6b2a99a1bd105850ef90a33
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-06 16:36:56 +0300

    Removed extra code from banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -167,7 +167,7 @@ const Banner = () => {
                   <motion.div whileTap={{ scale: 0.8 }}>
                     <a
                       href="./sayon-resume.pdf"
-                      className="btn bg-white hover:bg-white rounded-md shadow-md shadow-zinc-900 hover:text-orange-500 hover:shadow-orange-400 font-belano"
+                      className="btn bg-white hover:bg-white rounded-md shadow-md shadow-zinc-900 hover:text-orange-500 hover:shadow-orange-400"
                       download
                     >
                       Download Resume
@@ -177,7 +177,7 @@ const Banner = () => {
                   <motion.div whileTap={{ scale: 0.8 }}>
                     <a
                       href="#contact"
-                      className="btn bg-white hover:bg-white rounded-md shadow-md shadow-zinc-900 hover:text-orange-500 hover:shadow-orange-400 ml-2 sm:ml-5 lg:ml-9 font-belano"
+                      className="btn bg-white hover:bg-white rounded-md shadow-md shadow-zinc-900 hover:text-orange-500 hover:shadow-orange-400 ml-2 sm:ml-5 lg:ml-9"
                     >
                       Hire Me
                     </a>
